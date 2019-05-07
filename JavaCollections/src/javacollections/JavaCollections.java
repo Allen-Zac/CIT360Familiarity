42b886d7bb3bd7dece9ebe8971e89757d25c6c3f
Allen-Zac <all16059@byui.edu>
2019-05-07 01:48:37 -0600

    First test commit.

---
+++ JavaCollections/src/javacollections/JavaCollections.java
@@ -0,0 +1,13 @@
+package javacollections;
+
+/**
+ *
+ * @author Zac Allen
+ */
+public class JavaCollections {
+
+    public static void main(String[] args) {
+        System.out.println("Which collection would you like to run?");
+    }
+    
+}
