495e6164ed61554a6689d78abf51268af4b6bbe3
Allen-Zac <all16059@byui.edu>
2019-05-07 02:17:22 -0600

    Set up Switch for Java Collections.

--- JavaCollections/src/javacollections/JavaCollections.java
+++ JavaCollections/src/javacollections/JavaCollections.java
@@ -1,5 +1,7 @@
 package javacollections;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Zac Allen
@@ -7,7 +9,30 @@ package javacollections;
 public class JavaCollections {
 
     public static void main(String[] args) {
+        boolean running = true;
+        Scanner s = new Scanner(System.in);
+        
         System.out.println("Which collection would you like to run?");
+        
+        while(running == true){
+            
+            String input = s.nextLine();
+            
+            switch (input.trim().toUpperCase()) {
+            case "N":
+                System.out.println("Place holder for collection 1");
+                break;
+            case "L":
+                System.out.println("Place holder for collection 2");
+                break;
+            case "H":
+                System.out.println("Place holder for collection 3");
+                break;
+            case "Q": 
+                running = false;
+            }   
+        }
+        
     }
     
 }
