75df07b20d97d7d5875aaf02ad8b40a27c2f486c
Allen-Zac <all16059@byui.edu>
2019-05-23 17:34:08 -0600

    Created MVC Project

---
+++ MVC/src/mvc/MVC.java
@@ -0,0 +1,16 @@
+package mvc;
+
+/**
+ *
+ * @author Zac Allen
+ */
+public class MVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
