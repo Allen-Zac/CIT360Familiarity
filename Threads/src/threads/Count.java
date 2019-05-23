044fdd56193b1665905769bbad6553d526370944
Allen-Zac <all16059@byui.edu>
2019-05-23 17:15:06 -0600

    Finished Simple Threading Project.

--- Threads/src/threads/Count.java
+++ Threads/src/threads/Count.java
@@ -1,9 +1,28 @@
 package threads;
 
+import java.util.Random;
+
 /**
  *
  * @author Zac Allen
  */
-public class Count {
+public class Count implements Runnable {
+    String name;
+    int number;
+    Random rand = new Random();
     
+    public Count(String x) {
+        name = x;
+        number = rand.nextInt(10);
+    }
+    
+    @Override
+    public void run() {
+        System.out.printf("%s is counting to %d\n", name, number);
+        //Thread.sleep(number);
+        for (int i = 0; i < number; i++) {
+            System.out.printf("%s says: %d\n", name, i + 1);
+        }
+        System.out.printf("%s has finished counting!\n", name);
+    }
 }
