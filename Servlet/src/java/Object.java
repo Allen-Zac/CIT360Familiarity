e5b88576a9f69727648f83902f2c3f4672368e23
Allen-Zac <all16059@byui.edu>
2019-10-12 12:39:46 -0600

    Finished Servlet

---
+++ Servlet/src/java/Object.java
@@ -0,0 +1,34 @@
+/**
+ *
+ * @author Zac Allen
+ */
+public class Object {
+    private String foodName;
+    private String calories;
+    private String likeIt;
+
+
+    public String foodName() {
+            return foodName;
+    }
+
+    public void foodName(String foodName) {
+            this.foodName = foodName;
+    }
+
+    public String getCalories() {
+            return calories;
+    }
+
+    public void setCalories(String calories) {
+            this.calories = calories;
+    }
+
+    public String getLikeIt() {
+            return likeIt;
+    }
+
+    public void setLikeIt(String likeIt) {
+            this.likeIt = likeIt;
+    }
+}
